c53757514cb82b0092f64b553e6f51c4d0f31c79
WibyFabian08 <wibyfabian08@gmail.com>
2022-08-16 11:00:30 +0700

    design add-hero page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddHeroComponent } from './add-hero/add-hero.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: "dashboard",
     component: DashboardComponent
+  },
+  {
+    path: "add",
+    component: AddHeroComponent
   }
 ];
 
