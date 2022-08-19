3801843658eaad89ef01a3a1258bc8d44327d545
WibyFabian08 <wibyfabian08@gmail.com>
2022-08-19 10:17:53 +0700

    setup users routing

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: UsersComponent,
+    children: [
+      {
+        path: "login",
+        component: LoginComponent
+      },
+      {
+        path: "register",
+        component: RegisterComponent
+      },
+      {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "login"
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
