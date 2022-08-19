4691112a3b702544e6325eaea044e47fd82e620c
WibyFabian08 <wibyfabian08@gmail.com>
2022-08-19 10:14:34 +0700

    add users component

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { UsersComponent } from './users/users.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    UsersComponent
   ],
   imports: [
     CommonModule,
